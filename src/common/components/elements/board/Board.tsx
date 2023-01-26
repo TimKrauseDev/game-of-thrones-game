45b1675e7f911c0be7ccc12052d6ba8cb1ef5077
Tim Krause <timkrausedev@gmail.com>
2023-01-26 18:35:05 -0500

    add template for main board and statistics section

--- src/common/components/elements/board/Board.tsx
+++ src/common/components/elements/board/Board.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useRef } from 'react'
 
 function Board() {
-  return (
-    <div>
-        <p className="font-bold">Board</p>
-    </div>
-  )
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const boardRef = useRef<HTMLDivElement>(null)
+
+    function paintBoard() {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+        const context = canvas.getContext('2d');
+        if (!context) return;
+
+        const rows = 7;
+        const columns = 7;
+
+        const squares = [...new Array(rows)].map((a, i) =>
+            [...new Array(columns)].map((a, i) => { return { selected: false }; }));
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                squares[i][j].selected = true;
+            }
+        }
+        let width = boardRef.current?.offsetWidth || 0;
+        width = width <= 400 ? width : 400;
+        let height = width / columns * rows;
+
+        const squareWidth = Math.floor(width / columns);
+        const squareHeight = Math.floor(height / rows);
+        canvas.width = width;
+        canvas.height = height;
+
+
+        if (context) {
+            context.fillStyle = 'black';
+            context.fillRect(0, 0, squareWidth * columns + 1, squareHeight * rows + 1);
+            squares.forEach((line, i) => {
+                line.forEach((square, j) => {
+                    context.fillStyle = (square.selected) ? 'lightgreen' : '#E2E8F0';
+                    context.fillRect(squareWidth * j + 1, squareWidth * i + 1, squareWidth - 1, squareWidth - 1);
+                });
+            });
+        }
+    }
+
+    useEffect(() => {
+        paintBoard();
+        window.addEventListener('resize', paintBoard);
+        return () => { window.removeEventListener('resize', paintBoard) }
+    }, [])
+
+    return (
+        <div className="board space-y-2" ref={boardRef} >
+            <p className="font-bold">Board</p>
+            <canvas ref={canvasRef} className="m-auto" />
+        </div>
+    )
 }
 
-export default Board
+export default Board;
--- src/common/components/elements/statistics/Statistics.tsx
+++ src/common/components/elements/statistics/Statistics.tsx
@@ -1,11 +1,62 @@
 import React from 'react'
 
 function Statistics() {
-  return (
-    <div>
-        <p className="font-bold">Statistics</p>
-    </div>
-  )
+    return (
+        <div className="statistics space-y-2">
+            <p className="font-bold">Statistics</p>
+            <div className="bg-slate-300 rounded-md p-6 grid grid-cols-1 sm:grid-cols-3 gap-6">
+                <div className="hero-statistics space-y-2">
+                    <h2>Hero Statistics</h2>
+                    <div>
+                        <p><span className="font-bold">Name:</span> Tim Krause</p>
+                        <p><span className="font-bold">Money:</span> $1000</p>
+                        <p><span className="font-bold">Influence:</span> 100</p>
+                        <p><span className="font-bold">Army Size</span>: 2,000</p>
+                        <p><span className="font-bold">Points:</span> 3,035</p>
+                        <p><span className="font-bold">Ship:</span> Yes</p>
+                        <p><span className="font-bold">Percent of Spaces Controlled:</span> 0.15%</p>
+                    </div>
+                </div>
+                <div className="worrior-statistics col-span-2 space-y-2">
+                    <h2>Worrior statistics</h2>
+                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+                        <div className="border border-slate-700 rounded-md p-2 shadow-lg">
+                            <p><span className="font-bold">Name:</span> Worrior 1</p>
+                            <p><span className="font-bold">Strength:</span> 120</p>
+                            <p><span className="font-bold">Loyalty:</span> 120</p>
+                            <p><span className="font-bold">Ship:</span> Yes</p>
+                            <p><span className="font-bold">Strength:</span> 120</p>
+                            <p><span className="font-bold">Loyalty:</span> 120</p>
+                        </div>
+                        <div className="border border-slate-700 rounded-md p-2 shadow-lg">
+                            <p><span className="font-bold">Name:</span> Worrior 2</p>
+                            <p><span className="font-bold">Strength:</span> 120</p>
+                            <p><span className="font-bold">Loyalty:</span> 120</p>
+                            <p><span className="font-bold">Ship:</span> Yes</p>
+                            <p><span className="font-bold">Strength:</span> 120</p>
+                            <p><span className="font-bold">Loyalty:</span> 120</p>
+                        </div>
+                        <div className="border border-slate-700 rounded-md p-2 shadow-lg">
+                            <p><span className="font-bold">Name:</span> Worrior 3</p>
+                            <p><span className="font-bold">Strength:</span> 120</p>
+                            <p><span className="font-bold">Loyalty:</span> 120</p>
+                            <p><span className="font-bold">Ship:</span> Yes</p>
+                            <p><span className="font-bold">Strength:</span> 120</p>
+                            <p><span className="font-bold">Loyalty:</span> 120</p>
+                        </div>
+                        <div className="border border-slate-700 rounded-md p-2 shadow-lg">
+                            <p><span className="font-bold">Name:</span> N/a</p>
+                            <p><span className="font-bold">Strength:</span> N/a</p>
+                            <p><span className="font-bold">Loyalty:</span> N/a</p>
+                            <p><span className="font-bold">Ship:</span> N/a</p>
+                            <p><span className="font-bold">Strength:</span> N/a</p>
+                            <p><span className="font-bold">Loyalty:</span> N/a</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Statistics
+export default Statistics;
